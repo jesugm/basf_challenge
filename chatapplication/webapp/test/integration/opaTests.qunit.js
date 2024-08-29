4742c593a4008f20671c625f31361f936aa8c9b5
Jesús González Méndez <jesus.gonzalez@lorum4re.com>
2024-08-29 15:06:04 +0000

    project created

---
+++ chatapplication/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/basf/yardmanagement/chatapp/chatapplication/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ chatapplication/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"combasfyardmanagementchatapp/chatapplication/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
